859e09d469a254f5203b27e095626aa676e94375
Md Wasiul Islam <99739954+mislam77@users.noreply.github.com>
2025-03-22 11:39:35 -0400

    Initialized Shadcn UI & Added Button component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Welcome to RealEstate Listing Viewer</h1>
+    <div className="flex items-center justify-center min-h-screen flex-col">
+      <h1 className="text-4xl font-bold mb-4">Welcome to RealEstate Listing Viewer</h1>
+      <Button className="px-4 py-2 rounded">
+        Get Started
+      </Button>
     </div>
   );
 }
